7c8da6c3a950aa33f3420eaab282826cba59808c
zhoubk <zhoubk@zhoubkdeMacBook-Pro.local>
2023-09-06 09:04:06 +0800

    z增加登陆，前端优化，分页功能

--- Cloud_Album/cloud_photo/urls.py
+++ Cloud_Album/cloud_photo/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from cloud_photo.views import home
+from cloud_photo.views import home, upload
 
 # App名称
 # 用于Django幕后的url查询
@@ -8,4 +8,5 @@ app_name = 'photo'
 # url列表
 urlpatterns = [
     path('', home, name='home'),
+    path('upload/', upload, name='upload'),
 ]
--- Cloud_Album/cloud_photo/views.py
+++ Cloud_Album/cloud_photo/views.py
@@ -1,7 +1,37 @@
 from django.shortcuts import render
 from cloud_photo.models import Photo
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
+from django.core.paginator import Paginator
 
 def home(request):
-    photos  = Photo.objects.all()
-    context = {'photos': photos}
+    photos = Photo.objects.all()
+    # 新增分页代码
+    paginator    = Paginator(photos, 5)
+    page_number  = request.GET.get('page')
+    paged_photos = paginator.get_page(page_number)
+    # 将分页器对象传入上下文
+    context = {'photos': paged_photos}
+
+    # 处理登入登出的POST请求
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user     = authenticate(request, username=username, password=password)
+        # 登入
+        if user is not None and user.is_superuser:
+            login(request, user)
+        # 登出
+        isLogout  = request.POST.get('isLogout')
+        if isLogout == 'True':
+            logout(request)
+
     return render(request, 'cloud_photo/list.html', context)
+
+def upload(request):
+    if request.method == 'POST' and request.user.is_superuser:
+        images = request.FILES.getlist('images')
+        for i in images:
+            photo = Photo(image=i)
+            photo.save()
+    return redirect('home')
